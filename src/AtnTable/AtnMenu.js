2d25e7a4e0758afdd99302cc0692918e20eca51e
ATN554 <59557591+ATN554@users.noreply.github.com>
2020-03-22 17:29:39 +0300

    AtnMenu

---
+++ src/AtnTable/AtnMenu.js
@@ -0,0 +1,90 @@
+import React from "react";
+import getUID from "../../UID/uid.js";
+
+export default class AtnMenu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuId: getUID(),
+      show: false,
+      mainClass: props.mainClass,
+      contentClass: props.contentClass,
+      buttonClass: props.buttonClass
+    };
+
+    this.close = this.close.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.setShow = this.setShow.bind(this);
+
+    this.onMouseUp = this.onMouseUp.bind(this);
+    this.onTouchEnd = this.onTouchEnd.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("mouseup", this.onMouseUp);
+    document.addEventListener("touchend", this.onTouchEnd);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mouseup", this.onMouseUp);
+    document.removeEventListener("touchend", this.onTouchEnd);
+  }
+
+  onMouseUp(event) {
+    event.preventDefault();
+    let el = document.getElementById(this.state.menuId);
+    if (!el.contains(event.target)) {
+      this.close();
+    }
+  }
+
+  onTouchEnd(event) {
+    event.preventDefault();
+    if (event.changedTouches.length > 0) {
+      let touch = event.changedTouches[0];
+      let el = document.getElementById(this.state.menuId);
+      if (!el.contains(touch.target)) {
+        this.close();
+      }
+    }
+  }
+
+  close() {
+    this.setShow(false);
+  }
+
+  toggle() {
+    this.setShow(!this.state.show);
+  }
+
+  setShow(show) {
+    if (show) {
+      document.querySelector("." + this.state.mainClass).classList.add('active');
+      document.querySelector("." + this.state.contentClass).classList.add('active');
+    } else {
+      document.querySelector("." + this.state.mainClass).classList.remove('active');
+      document.querySelector("." + this.state.contentClass).classList.remove('active');
+    }
+    this.setState({show: show});
+  }
+
+  render() {
+    return (
+        <div
+          id={this.state.menuId}
+          className={this.state.mainClass}
+        >
+          <div className={this.state.contentClass}>
+            {this.props.children}
+          </div>
+          <input
+            type="button"
+            value=""
+            onClick={this.toggle}
+            className={this.state.buttonClass}
+      />
+        </div>
+    );
+  }
+}
--- src/AtnTable/AtnTable.js
+++ src/AtnTable/AtnTable.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./container.css";
+import "./menutop.css";
+import "./menubot.css";
 import getUID from "../UID/uid.js";
 import AtnContent from "./AtnContent.js";
+import AtnMenu from "./AtnMenu.js";
 
 const fillColumnsTableData = (columns) => {
   columns.forEach((column, column_idx) => {
@@ -19,8 +22,6 @@ export default class AtnTable extends React.Component {
   constructor(props) {
     super(props);
 
-    ;
-
     this.state = {
       columns: fillColumnsTableData(props.columns),
       data: props.data,
@@ -34,25 +35,35 @@ export default class AtnTable extends React.Component {
         <table className="atn-container">
           <thead className="atn-container-th">
             <tr className="atn-toolbar-tr">
-              <td colSpan={5} className="atn-toolbar">
-                <div style={{ height: "40px" }}>Toolbar</div>
+              <td colSpan={3} className="atn-toolbar">
+                <div style={{ height: "40px" }}>
+                  Панель кнопок
+                </div>
               </td>
             </tr>
             <tr className="atn-groupbar-tr">
-              <td colSpan={5} className="atn-groupbar">
-                <div style={{ height: "30px" }}>Groupbar</div>
+              <td colSpan={3} className="atn-groupbar">
+                <div style={{ height: "30px" }}>
+                  Панель группы
+                </div>
               </td>
             </tr>
           </thead>
           <tbody className="atn-container-tb">
             <tr className="atn-content-top-tr">
-              <td colSpan={5} className="atn-menu-top">
-                <div style={{ height: "30px" }}>Menu top</div>
+              <td colSpan={3} className="atn-menu-top">
+                <AtnMenu
+                  mainClass="atn-mtop"
+                  contentClass="atn-mtop-content"
+                  buttonClass="atn-mtop-button"
+                >
+                  Меню сверху
+                </AtnMenu>
               </td>
             </tr>
             <tr className="atn-content-mid-tr">
               <td className="atn-menu-left">
-                menu left
+                Меню слева
               </td>
               <td className="atn-content-mid">
                 <AtnContent
@@ -62,19 +73,27 @@ export default class AtnTable extends React.Component {
                 />
               </td>
               <td className="atn-menu-right">
-                menu right
+                Меню справа
               </td>
             </tr>
             <tr className="atn-content-bot-tr">
-              <td colSpan={5} className="atn-menu-bot">
-                <div style={{ height: "30px" }}>Menu bottom</div>
+              <td colSpan={3} className="atn-menu-bot">
+                <AtnMenu
+                  mainClass="atn-mbot"
+                  contentClass="atn-mbot-content"
+                  buttonClass="atn-mbot-button"
+                >
+                  Меню снизу
+                </AtnMenu>
               </td>
             </tr>
           </tbody>
           <tfoot className="atn-container-tf">
             <tr className="atn-container-tr">
-              <td colSpan={5} className="atn-footer">
-                <div style={{ height: "40px" }}>Footer</div>
+              <td colSpan={3} className="atn-footer">
+                <div style={{ height: "40px" }}>
+                  Подвал таблицы
+                </div>
               </td>
             </tr>
           </tfoot>
@@ -83,24 +102,3 @@ export default class AtnTable extends React.Component {
     );
   }
 }
-
-
-/*
-
-<div className="atn-table-container">
-          <AtnContent
-            columns={this.state.columns}
-            data={this.state.data}
-            resizer={this.state.resizer}
-          />
-
-          <div className="atn-tfoot">
-            <div className="atn-tfoot-tr">
-              <div className="atn-tfoot-td">
-                Footer
-                </div>
-            </div>
-          </div>
-        </div>
-
-*/
