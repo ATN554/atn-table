05c2e6121753716b35807acb66786feb9ad10aa3
ATN554 <59557591+ATN554@users.noreply.github.com>
2020-04-22 22:14:43 +0300

    setup examples

--- src/data.js
+++ src/data.js
@@ -1,104 +1,27 @@
-const columns = [
-  {
-    title: "Родит. Ид.",
-    field: "PARENT_ID",
-    type: "integer",
-    align: "right",
-    width: 150,
-    mimWidth: 10,
-    maxWidth: 250,
-    visibility: {
-      visible: false,
-    },
-  },
-  {
-    title: "Ид.",
-    field: "ID",
-    type: "integer",
-    align: "left",
-    width: 150,
-    mimWidth: 10,
-    maxWidth: 250,
-    visibility: {
-      visible: false,
-    },
-  },
-  {
-    title: "Сумма",
-    field: "SM",
-    type: "number",
-    align: "right",
-    width: 120,
-    visibility: {
-      visible: false
-    },
-    group: {
-      id: 0,
-      locked: true,
-    }
-  },
-  {
-    title: "Дата создания",
-    field: "DT_CREATE",
-    type: "date",
-    align: "center",
-    format: "DD.MM.YYYY",
-    width: 140,
-  },
-  {
-    title: "Исполнитель",
-    field: "URC",
-    type: "number",
-    width: 140,
-  }
-];
-
 export const groupColumns = [
-  {
-    title: "Наименование",
-    field: "FULL_NAME",
-    width: 250,
-    sort: {
-      order: "asc",
-    },
-  },
-  ...columns
+  { title: "Ид.", field: "ID", type: "integer", align: "left", width: 150, minWidth: 10, maxWidth: 250, visibility: { visible: false } },
+  { title: "Наименование", field: "FULL_NAME", width: 250, sort: { order: "asc" } },
+  { title: "Сумма", field: "SM", type: "number", align: "right", width: 120, visibility: { visible: false }, group: { locked: true } },
+  { title: "Дата создания", field: "DT_CREATE", type: "date", align: "center", format: "DD.MM.YYYY", width: 140 },
+  { title: "Исполнитель", field: "URC", type: "number", width: 140 }
 ];
 
 export const treeColumnsStatic = [
-  {
-    title: "Наименование",
-    field: "FULL_NAME",
-    width: 250,
-    tree: {
-      parentField: "PARENT_ID",
-      childField: "ID",
-      startFrom: 0,
-      load: false,
-    },
-    sort: {
-      order: "asc",
-    },
-  },
-  ...columns
+  { title: "Наименование", field: "FULL_NAME", width: 250, tree: {parentField: "PARENT_ID", childField: "ID", startFrom: 0, load: false}, sort: {order: "asc"} },
+  { title: "Родит. Ид.", field: "PARENT_ID", type: "integer", align: "right", width: 90, minWidth: 10, maxWidth: 250 },
+  { title: "Ид.", field: "ID", type: "integer", align: "left", width: 90, minWidth: 10, maxWidth: 250 },
+  { title: "Сумма", field: "SM", type: "number", align: "right", width: 120, visibility: { visible: false }, group: { locked: true } },
+  { title: "Дата создания", field: "DT_CREATE", type: "date", align: "center", format: "DD.MM.YYYY", width: 140 },
+  { title: "Исполнитель", field: "URC", type: "number", width: 140 }
 ];
 
 export const treeColumnsLoad = [
-  {
-    title: "Наименование",
-    field: "FULL_NAME",
-    width: 250,
-    tree: {
-      parentField: "PARENT_ID",
-      childField: "ID",
-      startFrom: 0,
-      load: true,
-    },
-    sort: {
-      order: "asc",
-    },
-  },
-  ...columns
+  { title: "Наименование", field: "FULL_NAME", width: 250, tree: { parentField: "PARENT_ID", childField: "ID", startFrom: 0, load: true }, sort: { order: "asc" } },
+  { title: "Родит. Ид.", field: "PARENT_ID", type: "integer", align: "right", width: 90, minWidth: 10, maxWidth: 250 },
+  { title: "Ид.", field: "ID", type: "integer", align: "left", width: 90, minWidth: 10, maxWidth: 250 },
+  { title: "Сумма", field: "SM", type: "number", align: "right", width: 120, visibility: { visible: false }, group: { locked: true } },
+  { title: "Дата создания", field: "DT_CREATE", type: "date", align: "center", format: "DD.MM.YYYY", width: 140 },
+  { title: "Исполнитель", field: "URC", type: "number", width: 140 }
 ];
 
 export const totals = {
